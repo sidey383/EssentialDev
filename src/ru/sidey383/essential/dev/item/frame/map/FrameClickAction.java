f936dce1469547b97c8b7c7e44f72c0735ed2182
NezuShin <73699833+NezuShin@users.noreply.github.com>
2021-05-19 14:44:08 +0500

    Update FrameClickAction.java

--- src/ru/sidey383/essential/dev/item/frame/map/FrameClickAction.java
+++ src/ru/sidey383/essential/dev/item/frame/map/FrameClickAction.java
@@ -1,5 +1,19 @@
 package ru.sidey383.essential.dev.item.frame.map;
 
 public enum FrameClickAction {
-	RIGHT_CLICK_BLOCK, RIGHT_CLICK_AIR, RIGHT_CLICK_FRAME, LEFT_CLICK_BLOCK, LEFT_CLICK_AIR, PHYSICAL, BLOCK_BREAK, FRAME_BREAK
+	RIGHT_CLICK_BLOCK(true), RIGHT_CLICK_AIR(true), RIGHT_CLICK_FRAME(true), LEFT_CLICK_BLOCK(false), LEFT_CLICK_AIR(false), PHYSICAL(false), BLOCK_BREAK(false), FRAME_BREAK(false);
+	
+	boolean isRight;
+	
+	FrameClickAction(boolean isRight) {
+		this.isRight = isRight;
+	}
+	
+	public boolean isRight() {
+		return isRight;
+	}
+	
+	public boolean isLeft() {
+		return !isRight;
+	}
 }
